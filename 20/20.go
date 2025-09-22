e8f2354e21e392706f9311a80034dd7b7d2096ec
Anthony <anthonyo@student.21-school.ru>
2025-09-22 21:38:57 +0300

    20

--- 20/20.go
+++ 20/20.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+Разработать программу, которая переворачивает порядок слов в строке.
+
+Пример: входная строка:
+
+«snow dog sun», выход: «sun dog snow».
+
+Считайте, что слова разделяются одиночным пробелом.
+Постарайтесь не использовать дополнительные срезы, а выполнять операцию «на месте».
+*/
+
+func reverseWords(s string) string {
+	words := strings.Fields(s)
+	n := len(words)
+	for i := 0; i < n/2; i++ {
+		words[i], words[n-1-i] = words[n-1-i], words[i]
+	}
+	return strings.Join(words, " ")
+}
+
+func main() {
+	inpt := "snow dog sun"
+	reversed := reverseWords(inpt)
+	fmt.Printf("Origin: %s\n", inpt)
+	fmt.Printf("Result: %s\n", reversed)
+}
