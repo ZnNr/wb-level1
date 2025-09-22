6b9c66cbb37f9413e4250c37c4822c5f182ff3eb
Anthony <anthonyo@student.21-school.ru>
2025-09-22 21:33:06 +0300

    19

--- 19/19.go
+++ 19/19.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+/*
+Разработать программу, которая переворачивает подаваемую на вход строку.
+
+Например: при вводе строки «главрыба» вывод должен быть «абырвалг».
+
+Учтите, что символы могут быть в Unicode (русские буквы, emoji и пр.),
+то есть просто iterating по байтам может не подойти — нужен срез рун ([]rune).
+
+*/
+
+func reverse(s string) string {
+	runes := []rune(s)
+	n := len(runes)
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	inpt := "golang"
+	result := reverse(inpt)
+	fmt.Printf("Origin: %s\n", inpt)
+	fmt.Printf("Result: %s\n", result)
+}
