c0b5016cf3f2f63509e604d62340e8edf8b2c585
Anthony <anthonyo@student.21-school.ru>
2025-09-25 23:04:24 +0300

    24

--- 24/24.go
+++ 24/24.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 /*
 Разработать программу нахождения расстояния между двумя точками на плоскости.
 Точки представлены в виде структуры Point с инкапсулированными (приватными) полями x, y (типа float64) и конструктором.
@@ -7,3 +12,23 @@ package main
 
 Подсказка: используйте функцию-конструктор NewPoint(x, y), Point и метод Distance(other Point) float64.
 */
+
+type Point struct {
+	x, y float64
+}
+
+func NewPoint(x, y float64) *Point {
+	return &Point{x: x, y: y}
+}
+
+func (p *Point) Distance(other *Point) float64 {
+	return math.Sqrt(math.Pow(other.x-p.x, 2) + math.Pow(other.y-p.y, 2))
+}
+
+func main() {
+	pointA := NewPoint(123.456, 6789)
+	pointB := NewPoint(223.456, 3789)
+	distance := pointA.Distance(pointB)
+	fmt.Printf("Расстояние между точками A(%.2f, %.2f) и B(%.2f, %.2f) равно %.2f\n",
+		pointA.x, pointA.y, pointB.x, pointB.y, distance)
+}
