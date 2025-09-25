cde89a20bcf6e19174f237c5edc84eb7230b4935
Anthony <anthonyo@student.21-school.ru>
2025-09-25 23:16:53 +0300

    26

--- 26/26.go
+++ 26/26.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 /*
 Разработать программу, которая проверяет, что все символы в строке встречаются один раз
 (т.е. строка состоит из уникальных символов).
@@ -11,3 +16,22 @@ package main
 
 Подумайте, какой структурой данных удобно воспользоваться для проверки условия.
 */
+func uniqSymbols(s string) bool {
+	charSet := make(map[rune]struct{})
+	s = strings.ToLower(s)
+	for _, char := range s {
+		if _, exists := charSet[char]; exists {
+			return false
+		}
+		charSet[char] = struct{}{}
+	}
+	return true
+}
+func main() {
+	testStrings := []string{"abcd", "abCdefAaf", "aabcd", "AaCcDd"}
+
+	for _, str := range testStrings {
+		result := uniqSymbols(str)
+		fmt.Printf("Строка: %s, Уникальные символы: %t\n", str, result)
+	}
+}
